8927bf761a740d0dd3791db976f005ef7f22b4c2
Dan Serban <dserban01@yahoo.com>
2014-07-04 11:26:38 +0300

    Add main :: IO () type signature

--- quickcheck/quickchecking_the_reverse_function.hs
+++ quickcheck/quickchecking_the_reverse_function.hs
@@ -3,5 +3,6 @@ import Test.QuickCheck
 propertyOfReverse :: [Int] -> [Int] -> Bool
 propertyOfReverse xs ys = reverse (xs ++ ys) == reverse ys ++ reverse xs
 
+main :: IO ()
 main = quickCheck propertyOfReverse
 
